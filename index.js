391f914632f7816006037b245f67182f875dfc10
sutej bali <sutejbali@gmail.com>
2023-11-02 19:15:32 +0530

    feat: api to bookmark image

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { http } from "@ampt/sdk";
 import express from "express";
 import imageGeneraterRoute from "./src/routes/imageGenerater.route.js";
+import bookmarkImageRoute from "./src/routes/bookmarkImage.route.js";
 
 const app = express();
 
@@ -11,6 +12,11 @@ app.use(express.json());
 // path prefix. This means that all routes defined in imageGeneraterRoute
 // will be accessible under '/api/image-generater' in the application.
 app.use("/api/image-generater", imageGeneraterRoute);
+
+// Mount the bookmarkImageRoute as a middleware using the '/api/bookmark/image'
+// path prefix. This means that all routes defined in bookmarkImageRoute
+// will be accessible under '/api/bookmark/image' in the application.
+app.use("/api/bookmark/image", bookmarkImageRoute);
 
 // Define a route that responds with Not Found when trying
 // to access end points not present
